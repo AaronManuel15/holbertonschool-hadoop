ec4f59fda7e96aced8e47e8fb018c4c402f974ca
AaronManuel15 <aaronmanuel15@gmail.com>
2023-11-07 14:26:22 -0600

    Task 1 edited to have comment code for adding one that doesnt already exist

--- hadoop_project/lao.sh
+++ hadoop_project/lao.sh
@@ -2,4 +2,6 @@
 # Task 1. Script that uploads the file lao.txt to the
 # /holbies/input directory
 hdfs dfs -copyFromLocal lao.txt /holbies/input
-# hdfs dfs -mv lao.txt /holbies/input
+
+# Can use the following to create a file if there isn't one locally
+# hdfs dfs -touch lao.txt /holbies/input
