7af842a2c56db8afd2f59cd7704ae5b82d4f3a13
AaronManuel15 <aaronmanuel15@gmail.com>
2023-11-07 18:47:12 -0600

    Task 4

---
+++ hadoop_project/5-deletedir.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+from snakebite.client import Client
+
+def deletedir(directory_list):
+    # Initialize Snakebite client
+    client = Client("localhost", 9000)
+
+    # sort directory list in reverse order
+    directory_list.sort(reverse=True)
+    try:
+        for x in client.rmdir(directory_list):
+            print(x)
+    except Exception as e:
+        print("Error: {}".format(e))
+
+if __name__ == '__main__':
+    directory_list = ["/Betty", "/Betty/Holberton"]
+    deletedir(directory_list)
